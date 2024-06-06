4a5a4ccb823e3edb17c7847292cb75b99b1cb227
Vincent Reau <vincent.reau1@gmail.com>
2024-06-06 17:23:52 +0200

    clean: proper formatting

---
+++ webscraping/importer.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*- #
+"""
+Importer of data
+"""
+
+import numpy
+
+
+class StockDataImporter:
+    """Import the data from Internet"""
+
+    def __init__(self) -> None:
+        pass
+    # End def __init__
+
+    def retrieve_data():
+        pass
+    # End def retrieve_data
+
+# End class StockDataImporter
