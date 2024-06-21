35c60c2216cfcca0b44569b881b3670f8dd4ef67
Vincent Reau <vincent.reau1@gmail.com>
2024-06-21 21:21:59 +0200

    refactor: :construction: Reformat the structure & add company class

--- stock_analysis/extraction/extraction.py
+++ stock_analysis/extraction/extraction.py
@@ -7,9 +7,10 @@ from __future__ import annotations
 
 import logging
 import pandas as pd
+from typing import Iterator
 from datetime import datetime
 
-from stock_analysis.fresh
+from stock_analysis.fresh_data.company import Company
 
 # ===========================================================================
 # Constant and global variables
@@ -29,21 +30,6 @@ class Extraction:
         # Raw data 
         self.tickers_info = kwargs.get('tickers_info', None)
         self.balance_sheets = kwargs.get('balance_sheets', None)
-        
-        # Bronze data
-        self.actual_share_price = None
-
-        self.sales = None
-        self.current_assets = None
-        self.current_liabilities = None
-        self.financial_debts = None
-        self.nb_shares_issued = None
-        self.equity = None
-        self.intangible_assets = None
-
-        self.net_income = None
-        self.dividends = None
-        self.net_earning_per_share = None
     # End def __init__
 
     # ===========================================================================
@@ -51,31 +37,37 @@ class Extraction:
     # ===========================================================================
 
     def __str__(self):
-        return f"Extraction({self.sales})"
+        return f"Extraction({datetime.now()} - {list(self.tickers_info.keys())})"
 
     # ===========================================================================
     # Public Methods
     # ===========================================================================
 
-    def extract_all(self) -> None:
+    def extract_all(self) -> Iterator[Company]:
+        # Extract data to company class
         for key in self.balance_sheets:
             logger.debug(f"Dataframe:\n{self.balance_sheets[key]}")
 
-            self.sales = self.__extract_sales()
-            
-            self.current_assets = self.__extract_current_assets(self.balance_sheets[key])
-            self.current_liabilities = self.__extract_current_liabilities(self.balance_sheets[key])
-            
-            self.financial_debts = self.__extract_long_term_debt(self.tickers_info[key])
-            
-            # Retrieve the number of shares
-            self.__extract_nb_shares_issued(key)
+            company = Company(
+                actual_share_price = 1,
+                sales              = self.__extract_sales(),
+                nb_shares_issued   = self.__extract_nb_shares_issued(key),
 
-            self.equity = self.__extract_equity(self.balance_sheets[key])
-            self.intangible_assets = self.__extract_goodwill(self.balance_sheets[key])
+                # Balance sheet information
+                current_assets      = self.__extract_current_assets(self.balance_sheets[key]),
+                current_liabilities = self.__extract_current_liabilities(self.balance_sheets[key]), 
+                financial_debts     = self.__extract_long_term_debt(self.tickers_info[key]),
+                equity              = self.__extract_equity(self.balance_sheets[key]),
+                intangible_assets   = self.__extract_goodwill(self.balance_sheets[key]), 
 
-        company = Company()
+                # Arrays
+                net_income=0,
+                dividends=0,   
+                net_earning_per_share=0
+            )
 
+            # Return company one by one
+            yield company
     # End def extract_all
 
     def as_rule_dataframe(self):
@@ -132,8 +124,8 @@ class Extraction:
         return result
     # End def __extract_long_term_debt
 
-    def __extract_nb_shares_issued(self, company: str) -> None:
-        self.nb_shares_issued = self.tickers_info[company]['nb_shares']
+    def __extract_nb_shares_issued(self, company: str) -> int:
+        return self.tickers_info[company]['nb_shares']
     # End def __extract_long_term_debt
 
     def __extract_equity(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -157,42 +149,15 @@ class Extraction:
     # End def __extract_goodwill
 
     def __extract_net_income(self, df: pd.DataFrame) -> pd.DataFrame:
-        try:
-            # Current liabilities index names
-            lookup_index = ['Current Liabilities', 'Other Current Liabilities']
-            
-            # Filter the DataFrame to keep only rows where the index is in look_index
-            result = df.loc[lookup_index]
-
-        except Exception as e:
-            result = None
-        return result
+        pass
     # End def __extract_net_income
 
     def __extract_dividends(self, df: pd.DataFrame) -> pd.DataFrame:
-        try:
-            # Current liabilities index names
-            lookup_index = ['Current Liabilities', 'Other Current Liabilities']
-            
-            # Filter the DataFrame to keep only rows where the index is in look_index
-            result = df.loc[lookup_index]
-
-        except Exception as e:
-            result = None
-        return result
+        pass
     # End def __extract_dividends
 
     def __extract_net_earning_per_share(self, df: pd.DataFrame) -> pd.DataFrame:
-        try:
-            # Current liabilities index names
-            lookup_index = ['Current Liabilities', 'Other Current Liabilities']
-            
-            # Filter the DataFrame to keep only rows where the index is in look_index
-            result = df.loc[lookup_index]
-
-        except Exception as e:
-            result = None
-        return result
+        pass
     # End def __extract_net_earning_per_share
 # End class Extraction
 
---
+++ stock_analysis/fresh_data/company.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*- #
+"""
+Company class
+"""
+
+from __future__ import annotations
+
+import logging
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+# ===========================================================================
+# Constant and global variables
+# ===========================================================================
+
+logger = logging.getLogger(__name__)
+
+# ===========================================================================
+# Company Class
+# ===========================================================================
+
+
+class Company:
+    """Represent a company"""
+
+    def __init__(self, **kwargs) -> None:
+        # metadata
+        self.name : str = kwargs.get('name', None)
+        self.last_update: datetime | None = datetime.now()
+
+        # Ticket info
+        self.actual_share_price  : float | None = kwargs.get('actual_share_price', None)
+        self.sales               : float | None = kwargs.get('sales', None)
+        self.nb_shares_issued    : int | None = kwargs.get('nb_shares_issued', None)
+
+        # Balance sheet information
+        self.current_assets      : float | None = kwargs.get('current_assets', None)
+        self.current_liabilities : float | None = kwargs.get('current_liabilities', None)
+        self.financial_debts     : float | None = kwargs.get('financial_debts', None)
+        self.equity              : float | None = kwargs.get('equity', None)
+        self.intangible_assets   : float | None = kwargs.get('intangible_assets', None)
+
+        # Arrays
+        self.net_income            : np.ndarray | None = kwargs.get('net_income', None)
+        self.dividends             : np.ndarray | None = kwargs.get('dividends', None)
+        self.net_earning_per_share : np.ndarray | None = kwargs.get('net_earning_per_share', None)
+    # End def __init__
+
+    # ===========================================================================
+    # Magic Methods
+    # ===========================================================================
+
+    def __str__(self):
+        return f"Company({self.name}: {self.last_update} - {self.actual_share_price}€)"
+
+    # ===========================================================================
+    # Accessors
+    # ===========================================================================
+
+    @property
+    def name(self) -> np.ndarray:
+        return self.name
+    # End def name
+
+    # ===========================================================================
+    # Public Methods
+    # ===========================================================================
+
+
+
+    # ===========================================================================
+    # Private Methods
+    # ===========================================================================
+# End class Company
